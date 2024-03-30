4313fd8337b81fe3b9138bbb12c796d797139975
Baraka Ahmed <baraka.ahmed@etu.univ-amu.fr>
2024-03-30 02:15:35 +0100

    Navigation entre ajouter Note et accueil

--- app/src/main/java/com/example/noteapp/AddNoteActivity.kt
+++ app/src/main/java/com/example/noteapp/AddNoteActivity.kt
@@ -2,24 +2,37 @@ package com.example.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AddNoteActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
 
-        // Au clique sur la toolbar
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        toolbar.setOnClickListener {
-            onBackPressed()
-            // Termine l'activité en cours et revient à l'activité précédente
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            finish()
         }
+
+        // Définir l'icône de navigation de la barre d'outils
+        toolbar.navigationIcon = ContextCompat.getDrawable(this, R.drawable.baseline_arrow_back_ios_24)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
+
--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.noteapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
@@ -7,16 +8,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var fab: ImageButton
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fab = findViewById(R.id.fab)
+        val fab: ImageButton = findViewById(R.id.fab)
         fab.setOnClickListener {
-            setContentView(R.layout.activity_add_note)
+            startActivity(Intent(this, AddNoteActivity::class.java))
         }
-
     }
 }
